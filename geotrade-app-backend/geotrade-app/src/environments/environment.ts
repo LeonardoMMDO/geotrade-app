608aa11d4e4d218553570aff22a9a79917506018
Eliud_Rivas_7 <eliudrvs4@gmail.com>
2026-04-04 21:22:17 -0600

    Agregando Dockerfile

--- geotrade-app-backend/geotrade-app/src/environments/environment.ts
+++ geotrade-app-backend/geotrade-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://geotrade-app.onrender.com',
   googleMapsApiKey: 'AIzaSyDz7JM4xzoAeUNjdPorkPCb1-KMTffXOuY'
 };
