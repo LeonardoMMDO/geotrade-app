f9e4e6939c6848a48eb110ed3270bad780fec584
Eliud_Rivas_7 <eliudrvs4@gmail.com>
2026-04-07 22:23:08 -0600

    Agregando Dockerfile

--- geotrade-app-backend/geotrade-app/src/main/java/com/geotrade/backend/service/CloudinaryService.java
+++ geotrade-app-backend/geotrade-app/src/main/java/com/geotrade/backend/service/CloudinaryService.java
@@ -3,9 +3,6 @@ package com.geotrade.backend.service;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +13,7 @@ import java.util.Map;
 public class CloudinaryService {
 
     @Autowired
-    private Cloudinary cloudinary;
+    private Cloudinary cloudinary; // Spring inyectará el Bean desde CloudinaryConfig
 
     @SuppressWarnings("rawtypes")
     public String uploadImage(MultipartFile file) throws IOException {
@@ -24,31 +21,10 @@ public class CloudinaryService {
             return null;
         }
         
+        // Sube el archivo y obtiene la respuesta de Cloudinary
         Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
         
+        // Retorna la URL segura (https)
         return uploadResult.get("secure_url").toString();
     }
-
-    @Configuration
-public class CloudinaryConfig {
-
-    @Value("${cloudinary.cloud_name}")
-    private String cloudName;
-
-    @Value("${cloudinary.api_key}")
-    private String apiKey;
-
-    @Value("${cloudinary.api_secret}")
-    private String apiSecret;
-
-    @Bean
-    public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", cloudName,
-            "api_key", apiKey,
-            "api_secret", apiSecret,
-            "secure", true
-        ));
-    }
-}
 }
